686758cd19c912800b4fba42003eff767bcb37c5
Maria907 <zefir105@yandex.ru>
2020-10-16 19:24:01 +0700

    aue

---
+++ lab_1_Melnikova_AVT_907.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "lab1_Melnikova.h"
+using namespace std;
+
+void Payment::set() {
+	*FIO = "Олегченко Олег Олегович";
+	dailySalary = 500;
+	employmentYear = 2012;
+	workedDays = 22;
+} // Функция класса Payment отвечающая за задание значий объекту
+
+void Payment::calculationSalary() {
+	salary = workedDays * dailySalary;
+} // Функция класса Payment отвечающая за расчет зарплаты работника
+
+void Payment::calculationPensionContributions() {
+	pensionContributions = salary * 0.01;
+} // Функция класса Payment отвечающая за расчет пенсионных вычетов работника
+
+void Payment::calculationIncomeTax() {
+	incomeTax = salary * 0.13;
+} // Функция класса Payment отвечающая за расчет подоходного налога работника
+
+void Payment::print() {
+	cout << "Данные сотрудника '" << *FIO << "'" << endl << "Оклад: " << dailySalary << " р." << endl << "Год постпуления на работу: " << employmentYear << endl << "Кол-во отработанных дней в месяце: " << workedDays << endl << "Зарплата: "<< salary << " р." << endl << "Отчисления в пенсионый фон: " << pensionContributions << " р." << endl << "Подоходный налог: " << incomeTax << " р." << endl;
+} // Функция класса Payment отвечающая за вывод объекта в консоль
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	Payment oleg; // Создания объекта "oleg"
+	oleg.set();
+	oleg.calculationSalary();
+	oleg.calculationPensionContributions();
+	oleg.calculationIncomeTax();
+	oleg.print();
+
+	cout << endl;
+	cin.get();
+	cin.get();
+}
---
+++ lab_1_Melnikova_AVT_907.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class Payment {
+	std::string* FIO = new std::string;
+	int dailySalary; // Переменная содержащая оклад работника
+	int employmentYear; // Переменная содержащая год принятия работника 
+	int workedDays; // Переменная содержащая отработанные работником дни за месяц 
+	float salary; // Переменная содержащая зарплату работника
+	float pensionContributions; // Переменная содержащая пенсионный вычет работника
+	float incomeTax; // Переменная содержащая подоходный налог работника
+
+public:
+	void set(); // Объявление функция класса Payment отвечающей за задание значений объекту
+	void calculationSalary(); // Объявление функция класса Payment отвечающей за расчет зарплаты работника
+	void calculationPensionContributions(); // Объявление функция класса Payment отвечающей за расчет пенсионных вычетов работника
+	void calculationIncomeTax(); // Объявление функция класса Payment отвечающей за расчет подоходного налога работника
+	void print(); // Объявление функция класса Payment отвечающей за вывод объекта в консоль
+
+}; // Объявление класса Payment
